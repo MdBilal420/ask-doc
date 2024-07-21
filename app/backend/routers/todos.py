3fc0a79751488f7f77f09a96c1931ca20a08ade7
Mohammad Bilal <mohdbilal16@gmail.com>
2024-07-21 07:26:05 +0530

    be: added langchain in routers

--- app/backend/routers/todos.py
+++ app/backend/routers/todos.py
@@ -6,6 +6,20 @@ import crud
 from database import SessionLocal
 from uuid import uuid4
 
+# Necessary imports for langchain summarization
+from langchain_core.prompts import  PromptTemplate
+from langchain_openai import OpenAI
+from langchain_core.output_parsers import StrOutputParser
+
+# Necessary imports to chat with a PDF file
+from langchain_community.document_loaders import PyPDFLoader
+from langchain.text_splitter import RecursiveCharacterTextSplitter
+from langchain_community.embeddings import OpenAIEmbeddings
+from langchain_community.vectorstores import FAISS
+from langchain.chains import RetrievalQA
+from schemas import QuestionRequest
+llm = OpenAI()
+
 router = APIRouter(prefix="/pdfs")
 
 def get_db():
@@ -47,3 +61,43 @@ def delete_pdf(id: int, db: Session = Depends(get_db)):
     if not crud.delete_pdf(db, id):
         raise HTTPException(status_code=404, detail="PDF not found")
     return {"message": "PDF successfully deleted"}
+
+
+# LANGCHAIN
+langchain_llm = OpenAI(temperature=0)
+
+summarize_template_string = """
+        Provide a summary for the following text:
+        {text}
+"""
+
+summarize_prompt = PromptTemplate(
+    template=summarize_template_string,
+    input_variables=['text'],
+)
+
+
+summarize_chain = summarize_prompt | langchain_llm | StrOutputParser()
+
+@router.post('/summarize-text')
+async def summarize_text(text: str):
+    summary = summarize_chain.invoke({"text":text})
+    return {'summary': summary}
+
+
+@router.post("/qa-pdf/{id}")
+def qa_pdf_by_id(id: int, question_request: QuestionRequest,db: Session = Depends(get_db)):
+    pdf = crud.read_pdf(db, id)
+    if pdf is None:
+        raise HTTPException(status_code=404, detail="PDF not found")
+    print(pdf.file)
+    loader = PyPDFLoader(pdf.file)
+    document = loader.load()
+    text_splitter = RecursiveCharacterTextSplitter(chunk_size=3000,chunk_overlap=400)
+    document_chunks = text_splitter.split_documents(document)
+    embeddings = OpenAIEmbeddings()
+    stored_embeddings = FAISS.from_documents(document_chunks, embeddings)
+    QA_chain = RetrievalQA.from_chain_type(llm=llm,chain_type="stuff",retriever=stored_embeddings.as_retriever())
+    question = question_request.question
+    answer = QA_chain.run(question)
+    return answer
