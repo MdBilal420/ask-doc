1d93f200e6fbb2d1734440cee4282717036b5899
Mohammad Bilal <mohdbilal16@gmail.com>
2024-07-21 10:59:44 +0530

    fe: QA from docs UI added

--- app/frontend/ask-docs/components/pdfs/pdf.tsx
+++ app/frontend/ask-docs/components/pdfs/pdf.tsx
@@ -4,7 +4,7 @@ import styles from '../../styles/pdf.module.css';
 export default function PDFComponent(props:any) {
   const { pdf, onChange, onDelete } = props;
   return (
-    <div className={styles.pdfRow}>
+    <div className="mt-6 flex ">
       <input
         className={styles.pdfCheckbox}
         name="selected"
@@ -24,12 +24,12 @@ export default function PDFComponent(props:any) {
         href={pdf.file}
         target="_blank"
         rel="noopener noreferrer"
-        className={styles.viewPdfLink}
+        className="bg-white shadow-sm ring-1 ring-inset ring-green-300 hover:ring-green-400 text-green-900 font-bold py-2 px-4 mx-2 rounded"
       >
         View
       </a>
       <button
-        className={styles.deleteBtn}
+        className="bg-white text-red-900 shadow-sm ring-1 ring-inset ring-red-300 hover:ring-red-400  font-bold py-2 px-4 mx-2 rounded"
         onClick={() => onDelete(pdf.id)}
       >
         Delete
